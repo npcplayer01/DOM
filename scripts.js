ccd0489d0bb6597739c5241f88d1643ec70f7ef3
Kevin Chavez <kevincha866@gmail.com>
2024-08-24 21:42:53 -0600

    Incorporación del array para almacenar los datos y las variables de selección de elementos del DOM

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Array para almacenar los datos
+let data = [];
+
+// Selección de elementos del DOM
+const dataForm = document.getElementById('data-form');
+const dataTable = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+const themeToggle = document.getElementById('theme-toggle');
